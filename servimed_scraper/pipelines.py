f13b538bbfba277b2d76d35211294a127b3277b0
João Vitor <gnomooooooo@gmail.com>
2026-02-12 20:07:12 -0300

    fix: readme e requirements arrumados, pipeline.py ajustada

--- servimed_scraper/pipelines.py
+++ servimed_scraper/pipelines.py
@@ -1,2 +1,16 @@
-# Define here the models for your item pipeline
-# Exportação JSON é feita via FEED no comando/script de execução
+
+class CollectItemsPipeline:
+    """
+    Pipeline que armazena os itens em uma lista (para uso pelo worker).
+    A lista é passada via settings['COLLECT_ITEMS_LIST'].
+    """
+    def __init__(self, items_list):
+        self.items_list = items_list
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(items_list=crawler.settings.get("COLLECT_ITEMS_LIST", []))
+
+    def process_item(self, item, spider):
+        self.items_list.append(dict(item))
+        return item
